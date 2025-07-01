1052cb4e2f2266380b0c48099321d72b2fa0e0c0
imlalitkashyap <officiallalit028@gmail.com>
2025-07-02 00:24:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "26640133"
+api_hash = "20f7033d806073ac2f4582e52e351d2e"
+bot_token = "8163799328:AAFmQD8rjTR604BlX7nyEWyUT6pmPiCvXGA"
+auth_users = [6477656662]
